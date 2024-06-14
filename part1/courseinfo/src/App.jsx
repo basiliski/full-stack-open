567b338242b45c694e787d716523eb3446a6e639
Lotta Lindell <lottalindell23@gmail.com>
2024-06-14 14:59:36 +0300

    exercises 1.1 and 1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+const Header = props => {
+  return (
+    <h1>{props.courseName}</h1>
+  )
+}
+
+const Part = props => {
+  return (
+    <p>
+        {props.part} {props.amount}
+    </p>
+  )
+}
+
+const Content = props => {
+  return (
+    <div>
+      <Part part={props.part1} amount={props.amount1}/>
+      <Part part={props.part2} amount={props.amount2}/>
+      <Part part={props.part3} amount={props.amount3}/>
+    </div>
+  )
+}
+
+const Total = props => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+  return (
+    <div>
+      <Header courseName={course}/>
+      <Content
+              part1={part1}
+              amount1={exercises1}
+              part2={part2}
+              amount2={exercises2}
+              part3={part3}
+              amount3={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
